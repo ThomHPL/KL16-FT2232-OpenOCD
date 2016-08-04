a5d80e16a762428630ff4210f24a2091cca3a56b
ThomHPL <thomas.herpoel@gmail.com>
2016-08-05 01:08:40 +0200

    path

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-start D:\Lab\openocd-0.9.0\bin\openOCD.exe -f KL16withFT2232.cfg
-start C:\Windows\System32\telnet.exe 127.0.0.1 4444
+start path\to\openOCD.exe -f KL16withFT2232.cfg
+start path\to\telnet.exe 127.0.0.1 4444
